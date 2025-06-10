9132a75da7249cb3019fef3b435e71e9979eae79
ca1zone <francescofgris@gmail.com>
2025-06-09 21:04:31 -0300

    rascunho do index

---
+++ pages/footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <section>
+        <div>
+            Desenvolvido por Francesco Gris
+        </div>
+        <div>
+            <a href="https://www.instagram.com/buffalosburgers.cm/">
+                <i class="fa-brands fa-instagram"></i>
+            </a>
+        </div>
+    </section>
+</footer>
---
+++ pages/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Design</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+    <link rel="stylesheet" href="style.css">
+    <script id="pixel-chaty" async="true" src="https://cdn.chaty.app/pixel.js?id=b4cJrVjh"></script>
+    <style>
+@import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300..700;1,300..700&family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+</style>
+</head>
+
+<body>
+    <?php
+    include 'nav.php';
+    ?>
+    <main>
+        <div class="mainft">
+            <img src="../img/ftmain.png">
+        </div>
+
+    </main>
+
+    <section class="container">
+        <button class="consulta">
+            Agendar Consulta
+        </button>
+    </section>
+
+    <section class="sobreall">
+        <div class="tltsobre">
+            <img src="../img/sobretlt.png">
+        </div>
+        <section class="sobre">
+            <div>
+                <img src="../img/img1.png">
+            </div>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam debitis nemo, quaerat rerum, impedit assumenda culpa blanditiis temporibus minima eius necessitatibus officia natus ratione veritatis similique itaque saepe deleniti aliquid.<br>
+                <br>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos porro, fuga beatae impedit perspiciatis voluptatibus labore architecto dignissimos possimus, dolorum, quo voluptatem incidunt nemo quae officiis illo dicta voluptates ab.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos porro, fuga beatae impedit perspiciatis voluptatibus labore architecto dignissimos possimus, dolorum, quo voluptatem incidunt nemo quae officiis illo dicta voluptates ab.
+            </div>
+        </section>
+    </section>
+    <?php
+    include 'footer.php';
+    ?>
+</body>
+
+</html>
