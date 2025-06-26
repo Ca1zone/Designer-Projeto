f8ebfd9fe7e4deb274f15e052bbb25e74a8dd0a9
ca1zone <francescofgris@gmail.com>
2025-06-26 13:49:18 -0300

    bugs de url

--- index.php
+++ index.php
@@ -1,15 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Design</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script id="pixel-chaty" async="true" src="https://cdn.chaty.app/pixel.js?id=b4cJrVjh"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300..700;1,300..700&family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+    </style>
+</head>
+<body>
+    
+
+<main>
 <?php 
 
 if (isset($_GET["param"])) {
     $param = $_GET["param"];
-    $p = explode("/", $param);
+    $p = explode("/", $_GET["param"]);
 }
 
-$pages = $p[0] ?? "home";
+$page = $p[0] ?? "home";
 
-if (file_exists($pages)) {
-    include $pages;
+$pagina = "pages/{$page}.php";
+
+if (file_exists($pagina)) {
+    include $pagina;
 } else {
     include "pages/erro.php";
 }
 ?>
+</main>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+ 
+</body>
+</html>
+
--- pages/erro.php
+++ pages/erro.php
@@ -1,5 +1,15 @@
 <div class="container">
-    <img src="../img/erro.jpg" alt="Página não encontrada">
-
-    <h2>A Página que você está tentando acessar não existe!</h2>
+    <p class="text-center" style="text-align: center;">
+    <img src="../img/erro.jpg" alt="Página não encontrada" style="width: 50%;">
+    </p>
+    <br>
+    <br>
+    <h2 class="text-center" style="font-family: 'Playfair Display', serif; text-align: center">
+        A página que você está tentando acessar não existe!
+    </h2>
+    <p class="text-center"style="text-align: center">
+        <a href="home" title="Página Inicial" class="btn btn-primary" >
+            Voltar para a página inicial
+        </a>
+    </p>
 </div>
